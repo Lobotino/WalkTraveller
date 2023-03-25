24a11007bc7802a7064bb762eed4a3a709bb652a
Lobotino <lobotino1@gmail.com>
2023-03-25 22:59:56 +0500

    Added privacy policy dialog with html text

---
+++ app/src/main/java/ru/lobotino/walktraveller/ui/PrivacyPolicyDialog.kt
@@ -0,0 +1,54 @@
+package ru.lobotino.walktraveller.ui
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.text.Html
+import android.text.method.LinkMovementMethod
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
+import ru.lobotino.walktraveller.R
+
+class PrivacyPolicyDialog(
+    context: Context
+) : Dialog(context) {
+
+    private val privacyPolicyText = context.getString(R.string.privacy_policy)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupWindowSettings()
+        setContentView(R.layout.privacy_policy_dialog)
+        setupCloseButton()
+        setupPrivacyPolicyText()
+    }
+
+    private fun setupWindowSettings() {
+        window?.setBackgroundDrawableResource(android.R.color.transparent)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+    }
+
+    private fun setupCloseButton() {
+        findViewById<Button>(R.id.close_button).apply {
+            setOnClickListener {
+                dismiss()
+            }
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    private fun setupPrivacyPolicyText() {
+        findViewById<TextView>(R.id.privacy_policy).apply {
+            text =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    Html.fromHtml(privacyPolicyText, Html.FROM_HTML_MODE_COMPACT)
+                } else {
+                    Html.fromHtml(privacyPolicyText)
+                }
+
+            movementMethod = LinkMovementMethod.getInstance()
+        }
+    }
+}
