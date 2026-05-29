8d6d2cd11851a8116e2eac46499023531908bd31
oksenofontov <oksenofontov@naumen.ru>
2026-05-29 09:44:28 +0000

    Create app-scoped analytics tracker in App

--- app/src/main/java/ru/lobotino/walktraveller/App.kt
+++ app/src/main/java/ru/lobotino/walktraveller/App.kt
@@ -3,14 +3,18 @@ package ru.lobotino.walktraveller
 import android.app.Application
 import android.os.StrictMode
 import android.preference.PreferenceManager
+import com.google.firebase.analytics.FirebaseAnalytics
 import org.osmdroid.config.Configuration
+import ru.lobotino.walktraveller.analytics.AnalyticsTracker
+import ru.lobotino.walktraveller.analytics.DebugLogAnalyticsLogger
+import ru.lobotino.walktraveller.analytics.FirebaseAnalyticsLogger
+import ru.lobotino.walktraveller.analytics.IAnalyticsLogger
+import ru.lobotino.walktraveller.analytics.IAnalyticsTracker
 
 class App : Application() {
 
-    companion object {
-        const val SHARED_PREFS_TAG = "walk_traveller_shared_prefs"
-        const val PATH_DATABASE_NAME = "walk_traveller"
-    }
+    lateinit var analyticsTracker: IAnalyticsTracker
+        private set
 
     override fun onCreate() {
         super.onCreate()
@@ -19,5 +23,18 @@ class App : Application() {
             applicationContext,
             PreferenceManager.getDefaultSharedPreferences(applicationContext)
         )
+        analyticsTracker = AnalyticsTracker(
+            buildList<IAnalyticsLogger> {
+                add(FirebaseAnalyticsLogger(FirebaseAnalytics.getInstance(this@App)))
+                if (BuildConfig.DEBUG) {
+                    add(DebugLogAnalyticsLogger())
+                }
+            }
+        )
+    }
+
+    companion object {
+        const val SHARED_PREFS_TAG = "walk_traveller_shared_prefs"
+        const val PATH_DATABASE_NAME = "walk_traveller"
     }
 }
